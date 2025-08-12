c4a87bc7b78f49f7e74476d81cb1a79f74233645
Anshika Singh 002 <llsweetweaponll@gmail.com>
2025-08-12 13:33:16 +0530

    added compiler and problem view and submit functionality

--- AUTH/frontend/src/components/Register.jsx
+++ AUTH/frontend/src/components/Register.jsx
@@ -1,37 +1,37 @@
-import React, {useState} from 'react';
-import {useNavigate} from 'react-router-dom';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import authService from '../services/authService';
 
 const Register = () => {
     const [formData, setFormData] = useState({
-        firstname:'',
-        lastname:'',
-        email:'',
-        password:'',
-        phoneno:''
+        firstname: '',
+        lastname: '',
+        email: '',
+        password: '',
+        phoneno: ''
     });
     const navigate = useNavigate();
 
-    const handleChange=(e)=>{
-        setFormData({...formData,[e.target.name]:e.target.value});
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
 
     };
 
-    const handleSubmit = async (e)=> {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        try{
+        try {
             await authService.register(
                 formData.firstname,
                 formData.lastname,
                 formData.email,
                 formData.password,
                 formData.phoneno
-                
+
             );
             console.log("Registration successful!");
             alert("Registration successful! You can now log in.");
             navigate('/login');
-        }catch(error){
+        } catch (error) {
             console.error("Registration failed:", error.response?.data || error.message);
             const errorMessage = error.response?.data?.message || "Something went wrong. Please try again.";
             alert("Registration failed:" + errorMessage);
@@ -39,16 +39,19 @@ const Register = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <h2>Register</h2>
-            <input type="text" name="firstname" value={formData.firstname} onChange={handleChange} placeholder="First Name" required />
-            <input type="text" name="lastname" value={formData.lastname} onChange={handleChange} placeholder="Last Name" required />
-            <input type="email" name="email" value={formData.email} onChange={handleChange} placeholder="Email" required />
-            <input type="password" name="password" value={formData.password} onChange={handleChange} placeholder="Password" required />
-            <input type="text" name="phoneno" value={formData.phoneno} onChange={handleChange} placeholder="Phone Number" required />
-            <button type="submit">Register</button>
-        </form>
+        <>
+            <h2 class="text-3xl font-bold underline">Register me haha</h2>
+
+            <form onSubmit={handleSubmit}>
+                <h2>Register</h2>
+                <input type="text" name="firstname" value={formData.firstname} onChange={handleChange} placeholder="First Name" required />
+                <input type="text" name="lastname" value={formData.lastname} onChange={handleChange} placeholder="Last Name" required />
+                <input type="email" name="email" value={formData.email} onChange={handleChange} placeholder="Email" required />
+                <input type="password" name="password" value={formData.password} onChange={handleChange} placeholder="Password" required />
+                <input type="text" name="phoneno" value={formData.phoneno} onChange={handleChange} placeholder="Phone Number" required />
+                <button type="submit">Register</button>
+            </form>
+        </>
     );
 };
-
 export default Register;
